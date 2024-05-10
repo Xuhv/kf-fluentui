91730ffb0f9f3cf96da7843ff11d2aaf8a516506
xuhui <xuhui_er@outlook.com>
2024-05-11 02:23:22 +0800

    try fix demo deploy

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "$fresh/server.ts"
-import tailwind from "$fresh/plugins/tailwind.ts"
+import { defineConfig } from "$fresh/server.ts";
+import tailwind from "$fresh/plugins/tailwind.ts";
 import { cssModules } from "https://deno.land/x/kf_fresh_plugins@v1.0.0-beta.2/plugins/cssModules.ts";
-import { resolve } from "$std/path/mod.ts"
+import { resolve } from "$std/path/mod.ts";
 
-export default defineConfig({
-  plugins: [
-    cssModules({
-      watchDir: resolve("./components"),
-      cssOutFile: resolve("./static/fui.css"),
-      tsOutDir: resolve("./components")
-    }),
-    tailwind()
-  ]
-})
+const IS_DENO_DEPLOY = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
+
+const plugins = [tailwind()];
+
+if (IS_DENO_DEPLOY) {
+  plugins.unshift(cssModules({
+    watchDir: resolve("./components"),
+    cssOutFile: resolve("./static/fui.css"),
+    tsOutDir: resolve("./components"),
+  }));
+}
+
+export default defineConfig({ plugins });
