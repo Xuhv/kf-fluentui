a75302d78f26ad19de474369bb2d4feed056c7f2
xuhui <xuhui_er@outlook.com>
2024-05-10 20:06:00 +0000

    build modules before start

---
+++ buildModules.ts
@@ -0,0 +1,15 @@
+import { cssModules } from "https://deno.land/x/kf_fresh_plugins@v1.0.0-beta.3/plugins/cssModules.ts";
+import { resolve } from "$std/path/mod.ts";
+
+const plugin = cssModules({
+    watchDir: resolve("./components"),
+    cssOutFile: resolve("./static/fui.css"),
+    tsOutDir: resolve("./components"),
+})
+
+export default plugin
+
+if (import.meta.main) {
+    await plugin.produceModules()
+    Deno.exit()
+}
--- fresh.config.ts
+++ fresh.config.ts
@@ -7,13 +7,8 @@ const IS_DENO_DEPLOY = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 const plugins = [tailwind()];
 
 if (!IS_DENO_DEPLOY) {
-  const cssModules = (await import("https://deno.land/x/kf_fresh_plugins@v1.0.0-beta.2/plugins/cssModules.ts")).cssModules
-
-  plugins.unshift(cssModules({
-    watchDir: resolve("./components"),
-    cssOutFile: resolve("./static/fui.css"),
-    tsOutDir: resolve("./components"),
-  }));
+  const cssModules = (await import("./buildModules.ts"))
+  plugins.unshift(cssModules as any);
 }
 
 export default defineConfig({ plugins });
