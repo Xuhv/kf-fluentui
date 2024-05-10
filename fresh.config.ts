3d7afc58eabeb89ad3e0e01086ceba77d5e201cd
xuhui <xuhui_er@outlook.com>
2024-05-10 18:50:36 +0000

    try fixing demo deploy again

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import { cssModules } from "https://deno.land/x/kf_fresh_plugins@v1.0.0-beta.2/plugins/cssModules.ts";
 import { resolve } from "$std/path/mod.ts";
 
 const IS_DENO_DEPLOY = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
@@ -8,6 +7,8 @@ const IS_DENO_DEPLOY = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 const plugins = [tailwind()];
 
 if (IS_DENO_DEPLOY) {
+  const cssModules = (await import("https://deno.land/x/kf_fresh_plugins@v1.0.0-beta.2/plugins/cssModules.ts")).cssModules
+
   plugins.unshift(cssModules({
     watchDir: resolve("./components"),
     cssOutFile: resolve("./static/fui.css"),
